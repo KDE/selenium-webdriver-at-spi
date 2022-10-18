b3a0cbb546bf6546379009eded1f98ab220e7acd
Harald Sitter <sitter@kde.org>
2022-10-18 23:42:29 +0200

    add missing require

--- run.rb
+++ run.rb
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # SPDX-FileCopyrightText: 2022 Harald Sitter <sitter@kde.org>
 
+require 'optparse'
+
 $stdout.sync = true # force immediate flushing without internal caching
 
 OptionParser.new do |opts|
