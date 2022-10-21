6d84a56d28766a3096e6715b77539a06cd0b0ebd
Harald Sitter <sitter@kde.org>
2022-10-21 14:42:33 +0200

    throw away old run. new is vastly superior

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# SPDX-License-Identifier: AGPL-3.0-or-later
-# SPDX-FileCopyrightText: 2021 Harald Sitter <sitter@kde.org>
-
-export FLASK_ENV=development
-export FLASK_APP=app.py
-flask run --port 4723
