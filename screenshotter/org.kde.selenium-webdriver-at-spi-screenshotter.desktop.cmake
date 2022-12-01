8a482ab070de1d3fba4159e6c4978af9aca1853f
Harald Sitter <sitter@kde.org>
2022-12-01 17:08:33 +0100

    typo--

--- screenshotter/org.kde.selenium-webdriver-at-spi-screenshotter.desktop.cmake
+++ screenshotter/org.kde.selenium-webdriver-at-spi-screenshotter.desktop.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: 2022 Harald Sitter <sitter@kde.org>
 [Desktop Entry]
-Name=Screenshot Authorizexport
+Name=Screenshot Authorize
 NoDisplay=true
 Exec=${CMAKE_INSTALL_PREFIX}/bin/selenium-webdriver-at-spi-screenshotter
 Type=Application
