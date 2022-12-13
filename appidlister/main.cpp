feff3bcf36ee67a80226f35b4e5e0692d9441d60
Harald Sitter <sitter@kde.org>
2022-12-13 12:41:36 +0100

    support appidlister on wayland
    by using libtaskmanager. this will require an env var to disable
    authorization checks on the kwin side.

--- appidlister/main.cpp
+++ appidlister/main.cpp
@@ -1,28 +1,40 @@
 // SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 // SPDX-FileCopyrightText: 2022 Harald Sitter <sitter@kde.org>
 
+#include <chrono>
+
 #include <QDebug>
 #include <QGuiApplication>
 #include <QJsonDocument>
 #include <QJsonObject>
+#include <QTimer>
 
-#include <KWindowInfo>
-#include <KWindowSystem>
+#include <taskmanager/abstracttasksmodel.h>
+#include <taskmanager/windowtasksmodel.h>
+
+using namespace std::chrono_literals;
 
 int main(int argc, char **argv)
 {
     const QGuiApplication app(argc, argv);
-    const auto wids = KWindowSystem::windows();
+
+    QTimer timer;
+    timer.setInterval(500ms);
+    TaskManager::WindowTasksModel model;
+    QObject::connect(&model, &QAbstractItemModel::dataChanged, &timer, QOverload<>::of(&QTimer::start));
+    QObject::connect(&timer, &QTimer::timeout, &app, &QCoreApplication::quit);
+    timer.start();
+
+    app.exec();
+
     QVariantHash pidsToAppIds;
-    for (const auto &wid : wids) {
-        const KWindowInfo info(wid, NET::WMPid, NET::WM2DesktopFileName | NET::WM2GTKApplicationId);
-        if (!info.desktopFileName().isEmpty()) {
-            pidsToAppIds.insert(QString::number(info.pid()), info.desktopFileName());
-        }
-        if (!info.gtkApplicationId().isEmpty()) {
-            pidsToAppIds.insert(QString::number(info.pid()), info.gtkApplicationId());
-        }
+    const auto count = model.rowCount();
+    for (auto i = 0; i < count; ++i) {
+        const auto index = model.index(i, 0);
+        pidsToAppIds.insert(model.data(index, TaskManager::AbstractTasksModel::AppPid).toString(),
+                            model.data(index, TaskManager::AbstractTasksModel::AppId));
     }
     const QJsonDocument doc(QJsonObject::fromVariantHash(pidsToAppIds));
     printf("%s\n", doc.toJson().constData());
+    return 0;
 }
