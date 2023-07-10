b828c6cef3d42e2404b6015098a3785b411fb74b
Nicolas Fella <nicolas.fella@gmx.de>
2023-07-10 23:24:38 +0200

    Add missing include

--- screenshotter/main.cpp
+++ screenshotter/main.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 // SPDX-FileCopyrightText: 2022 Harald Sitter <sitter@kde.org>
 
+#include <array>
 #include <chrono>
 #include <optional>
 
