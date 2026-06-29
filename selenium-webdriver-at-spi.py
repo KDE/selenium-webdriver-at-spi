d45a21e8f1b3591dc921f0be85f1ecd834cbe413
Nicolas Fella <nicolas.fella@gmx.de>
2026-06-29 19:16:15 +0200

    Fix focus action name with Qt 6.12
    https://codereview.qt-project.org/c/qt/qtbase/+/727203 changed
    the action name from "SetFocus" to "Set Focus".
    
    Accept both.

--- selenium-webdriver-at-spi.py
+++ selenium-webdriver-at-spi.py
@@ -454,6 +454,10 @@ def session_element_click(session_id, element_id):
         action.doAction(availableActions['SetFocus'])
         time.sleep(EVENTLOOP_TIME)
 
+    if 'Set Focus' in keys: # alternative name for above
+        action.doAction(availableActions['Set Focus'])
+        time.sleep(EVENTLOOP_TIME)
+
     try:
         if 'Press' in keys:
             action.doAction(availableActions['Press'])
@@ -618,7 +622,7 @@ def session_element_value(session_id, element_id):
         action = element.queryAction()
         processed = False
         for i in range(0, action.nActions):
-            if action.getName(i) == 'SetFocus':
+            if action.getName(i) == 'SetFocus' or action.getName(i) == 'Set Focus':
                 processed = True
                 action.doAction(i)
                 time.sleep(EVENTLOOP_TIME) # give the focus time to apply
@@ -676,7 +680,7 @@ def session_element_clear(session_id, element_id):
         action = element.queryAction()
         processed = False
         for i in range(0, action.nActions):
-            if action.getName(i) == 'SetFocus':
+            if action.getName(i) == 'SetFocus' or action.getName(i) == 'Set Focus':
                 processed = True
                 action.doAction(i)
                 time.sleep(EVENTLOOP_TIME) # give the focus time to apply
